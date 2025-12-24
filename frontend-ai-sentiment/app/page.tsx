49830927094ba6ab5db40d634a8b64966f02cda2
ZizahAwaliyah <azizahawaliyahjijah@gmail.com>
2025-12-24 21:41:27 +0800

    feat : update API URL

--- frontend-ai-sentiment/app/page.tsx
+++ frontend-ai-sentiment/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://project-ai-sentiment-kamu.vercel.app/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
